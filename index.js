fa52d1e737c7b952b186d205adab0c60684419bd
Sean Hasenstein <seanhasenstein@gmail.com>
2019-03-15 22:08:32 -0500

    add express

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
+const express = require("express");
 const getData = require("./src/api");
 
-// const indvForm19 =
-//   "https://officialsconnection.org/wp-json/gf/v2/forms/11/entries";
-const crewForm19 =
-  "https://officialsconnection.org/wp-json/gf/v2/forms/12/entries";
+const indvForm =
+  "https://officialsconnection.org/wp-json/gf/v2/forms/11/entries";
+const crewForm =
+  "https://officialsconnection.org/wp-json/gf/v2/forms/12/entries?paging[page_size]=20";
 const username = process.env.CONSUMER_KEY;
 const password = process.env.CONSUMER_SECRET;
 
-async function go() {
-  const data = await getData(crewForm19, username, password);
-  console.log(data);
-}
+const app = express();
 
-go();
+app.get("/api", async (req, res) => {
+  console.log("Getting the api data...");
+
+  const results = await getData(crewForm, username, password);
+
+  res.json(results.data.entries[0]);
+});
+
+app.listen(2019, () => {
+  console.log("App running on port 2019");
+});
